4cc08f43f95bf82ccb5e7a7bf86d3072d0ceb4b6
KienLe <23520797@gm.uit.edu.vn>
2024-10-06 19:11:46 +0700

    Add multiple startup project

--- DieuKhienMay/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ DieuKhienMay/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097cd50a98e969bc0923e2e2239968d03f68300b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb088d502f975a88a95bcb8655386338110d95d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
