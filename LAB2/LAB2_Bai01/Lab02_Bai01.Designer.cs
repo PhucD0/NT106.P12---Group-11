d2beb843511b119dde58b877d2165fc9825a7c98
Quoocode <vanquoc05072005@gmail.com>
2024-10-13 12:19:17 +0700

    Complete Lab02_Bai01

--- LAB2/LAB2_Bai01/Lab02_Bai01.Designer.cs
+++ LAB2/LAB2_Bai01/Lab02_Bai01.Designer.cs
@@ -29,24 +29,71 @@
         private void InitializeComponent()
         {
             this.btnExit = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.btnWriteFIle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(583, 336);
+            this.btnExit.Location = new System.Drawing.Point(38, 176);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(155, 56);
             this.btnExit.TabIndex = 0;
             this.btnExit.Text = "Thoát";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(230, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(658, 538);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(38, 13);
+            this.btnReadFile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(155, 59);
+            this.btnReadFile.TabIndex = 2;
+            this.btnReadFile.Text = "Đọc File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // btnWriteFIle
+            // 
+            this.btnWriteFIle.Location = new System.Drawing.Point(38, 94);
+            this.btnWriteFIle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnWriteFIle.Name = "btnWriteFIle";
+            this.btnWriteFIle.Size = new System.Drawing.Size(155, 57);
+            this.btnWriteFIle.TabIndex = 3;
+            this.btnWriteFIle.Text = "Ghi File";
+            this.btnWriteFIle.UseVisualStyleBackColor = true;
+            this.btnWriteFIle.Click += new System.EventHandler(this.btnWriteFIle_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Lab02_Bai01
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.btnWriteFIle);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnExit);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Lab02_Bai01";
             this.Text = "Lab02_Bai01";
             this.ResumeLayout(false);
@@ -56,5 +103,10 @@
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Button btnWriteFIle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
--- LAB2/LAB2_Bai01/Lab02_Bai01.cs
+++ LAB2/LAB2_Bai01/Lab02_Bai01.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,35 @@ namespace LAB2_Bai01
             var form1 = (Form1)Tag;
             form1.Show();
             Close();
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void btnWriteFIle_Click(object sender, EventArgs e)
+        {
+            string textUpper = richTextBox1.Text.ToUpper();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.WriteLine(textUpper);
+                }
+
+                using (StreamReader sr = new StreamReader(saveFileDialog1.FileName))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+            }
         }
     }
 }
